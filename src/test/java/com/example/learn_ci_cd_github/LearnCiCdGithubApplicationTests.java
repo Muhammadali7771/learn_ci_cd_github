7e83d6daf33b78ee244c5042a4f2496a9f2d81db
Muhammadali <akbarovmuhammadali777@gmail.com>
2025-10-01 23:37:27 +0500

    first commit

---
+++ src/test/java/com/example/learn_ci_cd_github/LearnCiCdGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_ci_cd_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnCiCdGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
